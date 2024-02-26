853b468bae61951645d44a2d0bb3fb754c5be894
Oliver Alvarado <153557532+oliveroalvarado@users.noreply.github.com>
2024-02-26 11:49:58 -0800

    working game

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-console.log("HELLO PAPA PLATOON!")
-// Your function(s) should go here that will interact with the webpage or DOM
+
+// let whiskey = getElementById('whiskey')
+//     console.log("HELLO WHISKEY PLATOON!")
+
+CorrectNum = () => {
+    let random = Math.floor(Math.random() * 10) + 1
+    console.log(random)
+    let user = document.getElementById('user').value
+    console.log(user)
+    if (user === random) {
+        document.getElementById('results').innerHTML = 'Correct'
+    } else {
+        document.getElementById('results').innerHTML = 'Try again'
+    }
+}
