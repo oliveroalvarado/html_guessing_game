8df1e4cab5ae79f7a65197d6cbb08453cd44ac40
Oliver Alvarado <153557532+oliveroalvarado@users.noreply.github.com>
2024-02-26 12:29:45 -0800

    working solution

--- app.js
+++ app.js
@@ -3,13 +3,21 @@
 //     console.log("HELLO WHISKEY PLATOON!")
 
 CorrectNum = () => {
-    let random = Math.floor(Math.random() * 10) + 1
+    getRandom = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    let random = getRandom(1, 4)
     console.log(random)
+    
     let user = document.getElementById('user').value
     console.log(user)
-    if (user === random) {
+    if (parseInt(user) === random) {
         document.getElementById('results').innerHTML = 'Correct'
+        document.getElementById('wrong').innerHTML = ''
     } else {
-        document.getElementById('results').innerHTML = 'Try again'
+        document.getElementById('results').innerHTML = ''
+        document.getElementById('wrong').innerHTML = 'Try again'
     }
 }
